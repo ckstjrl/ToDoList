83c4aa13f0ca7d3838374d83339cf77dc956c6b0
ckstjrl <qkrckstjr7876@gmail.com>
2026-03-04 14:31:45 +0900

    edit MainPage Files

--- Views/MainPage.xaml.cs
+++ Views/MainPage.xaml.cs
@@ -1,28 +1,52 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.ObjectModel;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using ToDoList.Models;
 
 namespace ToDoList.Views
 {
-    /// <summary>
-    /// MainPage.xaml에 대한 상호 작용 논리
-    /// </summary>
     public partial class MainPage : Page
     {
+        public ObservableCollection<TodoItem> TodoItems { get; set; }
+
         public MainPage()
         {
             InitializeComponent();
+
+            TodoItems = new ObservableCollection<TodoItem>();
+
+            // 테스트 데이터
+            TodoItems.Add(new TodoItem
+            {
+                Id = 1,
+                Title = "Git 협업 연습",
+                Memo = "CRUD 연결 테스트"
+            });
+
+            TodoItems.Add(new TodoItem
+            {
+                Id = 2,
+                Title = "WPF 메인 화면 만들기"
+            });
+
+            DataContext = this;
+        }
+
+        // 카드 클릭 → READ 창
+        private void Todo_Click(object sender, RoutedEventArgs e)
+        {
+            Button btn = sender as Button;
+            TodoItem todo = btn.DataContext as TodoItem;
+
+            var readWindow = new ReadWindow(todo);
+            readWindow.Show();
+        }
+
+        // Add 버튼 → CREATE 창
+        private void Add_Click(object sender, RoutedEventArgs e)
+        {
+            var createWindow = new CreateWindow();
+            createWindow.Show();
         }
     }
 }
