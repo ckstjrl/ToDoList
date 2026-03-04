88e67cf604698db96516a3c21c2080193f668ee5
An Jongwon <ahn.jw.1116@gmail.com>
2026-03-04 12:13:56 +0900

    add Models

---
+++ Models/TodoItem.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ToDoList.Models
+{
+    public class TodoItem
+    {
+        // 각 일정을 구분하기 위한 고유 ID (C,R,U,D 연동 시 필수)
+        public int Id { get; set; }
+
+        // 제목
+        public string Title { get; set; }
+
+        // 시작일
+        public DateTime StartDate { get; set; }
+
+        // 마감일
+        public DateTime EndDate { get; set; }
+
+        // 메모
+        public string Memo { get; set; }
+
+        // 기본 생성자 (현재 시간으로 날짜 초기화)
+        public TodoItem()
+        {
+            StartDate = DateTime.Now;
+            EndDate = DateTime.Now.AddDays(1);
+        }
+    }
+}
